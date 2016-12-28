4cb47130303b06d800a60f058c4b27514b57de89
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2016-12-28 10:45:49 +0100

    Initial Commit

---
+++ PhotoStickers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoStickers
+//
+//  Created by Jochen Pfeiffer on 28/12/2016.
+//  Copyright © 2016 Jochen Pfeiffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
