a12e185961467f22a4b9ef5927bee8820ac4074f
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-01-01 21:28:06 +0100

    small adjustment to core data stack

---
+++ Shared/Sticker+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Sticker+Extensions.swift
+//  PhotoStickers
+//
+//  Created by Jochen Pfeiffer on 01/01/2017.
+//  Copyright © 2017 Jochen Pfeiffer. All rights reserved.
+//
+
+import Foundation
+
+extension Sticker {
+}
