9b46d413f036f98479ba3102f906cef2222ca514
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2017-02-06 21:09:49 +0100

    masks stickers to circle

--- App/Services/StickerRenderer.swift
+++ App/Services/StickerRenderer.swift
@@ -18,6 +18,8 @@ struct StickerRenderer {
         let cropBounds = sticker.cropBounds
         image = image.croppedImage(cropBounds)
         image = image.resizedImageWithContentMode(.scaleAspectFit, bounds: Sticker.renderedSize, interpolationQuality: .high)
+        let cornerRadius = Int(floor(min(image.size.width, image.size.height) / 2))
+        image = image.roundedCornerImage(cornerSize: cornerRadius, borderSize: 0) // todo: better handling of masks
         sticker.renderedSticker = image
     }
 }
