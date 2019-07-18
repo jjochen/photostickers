c121414520c1d48194972ea12e15e6bdaab7c2d2
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2019-07-18 17:21:53 +0200

    add test host, improve preinstall

---
+++ UITestHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestHost
+//
+//  Created by Jochen on 18.07.19.
+//  Copyright © 2019 Jochen Pfeiffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
